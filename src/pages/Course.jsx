c8965d9366f62461bb741d317e5324f8cc07c73f
davidrorez <126507534+davidrorez@users.noreply.github.com>
2025-12-18 13:44:06 -0600

    refactor: replace anchor tag with Link component in Product; add useLayoutEffect for scroll restoration in CoursePage

--- src/pages/Course.jsx
+++ src/pages/Course.jsx
@@ -1,3 +1,4 @@
+import { useLayoutEffect  } from "react"
 import {  useParams } from "react-router-dom"
 import CourseHeader from "../components/Course/CourseHeader"
 import CourseDescription from "../components/Course/CourseDescription"
@@ -6,6 +7,12 @@ import { coursesInfo } from "../utils/course-info"
 import ConsultForm from "../components/ConsultForm/ConsultForm"
 
 export default function CoursePage() {
+
+
+          useLayoutEffect (() => {
+    window.scrollTo(0, 0);
+  }, []);
+
     const { paramName } = useParams();
     const courseExists = coursesInfo.some((course) => course.paramName === paramName);  
 
